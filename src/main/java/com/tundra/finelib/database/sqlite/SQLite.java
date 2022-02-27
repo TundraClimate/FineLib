0f05949c39ba542a1a3b8c21aaa41cb45fbc8b33
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 02:13:00 +0900

    SQLite Connector

---
+++ src/main/java/com/tundra/finelib/database/sqlite/SQLite.java
@@ -0,0 +1,90 @@
+package com.tundra.finelib.database.sqlite;
+
+import com.tundra.finelib.FineLib;
+
+import javax.annotation.Nonnull;
+import java.sql.*;
+
+/**
+ * SQLite connect
+ */
+public class SQLite {
+    private Connection connect = null;
+    private Statement state = null;
+
+    /**
+     * connect Database
+     * @param dbname DatabaseName
+     */
+    public void connectSQLIte(@Nonnull final String dbname) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.connect = DriverManager.getConnection("jdbc:sqlite:" + ".\\plugins\\" + FineLib.getPlugin().getName() + "\\" + dbname);
+            this.state = connect.createStatement();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Wrapping executeUpdate
+     * @param sql SQL code
+     * @throws SQLException SQLException
+     */
+    public void executeUpdate(@Nonnull String sql) throws SQLException {
+        state.executeUpdate(sql);
+    }
+
+    /**
+     * Wrapping executeQuery
+     * @param sql SQL code
+     * @throws SQLException SQLException
+     * @return Query
+     */
+    public ResultSet executeQuery(@Nonnull String sql) throws SQLException {
+        return state.executeQuery(sql);
+    }
+
+    /**
+     * state getter
+     * @return state
+     */
+    public Statement getState() {
+        return state;
+    }
+
+    /**
+     * check Table
+     * @param table Table to check
+     * @return Does it exist
+     */
+    public boolean hasTable(@Nonnull String table) {
+        try {
+            ResultSet rs = state.executeQuery(
+                    "SELECT COUNT(*) FROM sqlite_master WHERE TYPE='table' AND name='" + table + "'"
+            );
+            if (rs.getInt(1) == 0)
+                return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * disconnect Database
+     */
+    public void disconnectSQLite() {
+
+        try {
+            if (connect != null) {
+                connect.close();
+            }
+            if (state != null) {
+                state.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
