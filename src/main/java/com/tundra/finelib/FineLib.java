ff84f5d51e652e452d31fb0ac95cebc416594f75
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 02:19:49 +0900

    MainClass UpData

--- src/main/java/com/tundra/finelib/FineLib.java
+++ src/main/java/com/tundra/finelib/FineLib.java
@@ -1,8 +1,6 @@
 package com.tundra.finelib;
 
-import com.tundra.finelib.database.sqlite.SQLite;
 import com.tundra.finelib.event.spigot.ChatEventListener;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
@@ -15,12 +13,16 @@ public final class FineLib {
 
     /**
      * Plugin getter
+     *
      * @return plugin instance
      */
-    public static JavaPlugin getPlugin() {return plugin;}
+    public static JavaPlugin getPlugin() {
+        return plugin;
+    }
 
     /**
      * set Plugin instance
+     *
      * @param plugin JavaPlugin instance for plugins that use FineLib
      */
     public static void setPlugin(@Nonnull final JavaPlugin plugin) {
@@ -28,29 +30,42 @@ public final class FineLib {
         registeredEvent(plugin);
     }
 
-    private static void registeredEvent(JavaPlugin plugin){
-        plugin.getServer().getPluginManager().registerEvents(new ChatEventListener(),plugin);
+    private static void registeredEvent(JavaPlugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(new ChatEventListener(), plugin);
     }
 
     /**
      * logging to test
      */
-    public static void testLogging(){getPlugin().getLogger().info("--- test Logging ---");}
+    public static void testLogging() {
+        getPlugin().getLogger().info("--- test Logging ---");
+    }
+
     /**
      * logging to message
+     *
      * @param message logging Message
      */
-    public static void testLogging(String message){getPlugin().getLogger().info(message);}
+    public static void testLogging(String message) {
+        getPlugin().getLogger().info(message);
+    }
+
     /**
      * test Log to the plugin console
+     *
      * @param plugin logging plugin
      */
-    public static void testLogging(JavaPlugin plugin){plugin.getLogger().info("--- test Logging ---");}
+    public static void testLogging(JavaPlugin plugin) {
+        plugin.getLogger().info("--- test Logging ---");
+    }
 
     /**
      * message Log to the plugin console
+     *
      * @param message logging Message
-     * @param plugin logging Plugin
+     * @param plugin  logging Plugin
      */
-    public static void testLogging(String message, JavaPlugin plugin){plugin.getLogger().info(message);}
+    public static void testLogging(String message, JavaPlugin plugin) {
+        plugin.getLogger().info(message);
+    }
 }
