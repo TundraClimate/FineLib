3e401945a4a17ca95a4e32a40f16828b3e9cbf9c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-10 01:53:59 +0900

    PlayerInteractEvent Detect

---
+++ src/main/java/com/tundra/finelib/event/spigot/PlayerInteract.java
@@ -0,0 +1,30 @@
+package com.tundra.finelib.event.spigot;
+
+import com.tundra.finelib.FineLib;
+import com.tundra.finelib.event.SignClickEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class PlayerInteract implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if (e.getClickedBlock() == null) {
+            return;
+        }
+        switch (e.getClickedBlock().getType()) {
+            case ACACIA_SIGN, ACACIA_WALL_SIGN,
+                    SPRUCE_SIGN, SPRUCE_WALL_SIGN,
+                    BIRCH_SIGN, BIRCH_WALL_SIGN,
+                    OAK_SIGN, OAK_WALL_SIGN,
+                    DARK_OAK_SIGN, DARK_OAK_WALL_SIGN,
+                    JUNGLE_SIGN, JUNGLE_WALL_SIGN,
+                    CRIMSON_SIGN, CRIMSON_WALL_SIGN,
+                    WARPED_SIGN, WARPED_WALL_SIGN -> FineLib.syncRunTask(() ->
+                    Bukkit.getPluginManager().callEvent(
+                            new SignClickEvent(e.getPlayer(), e.getClickedBlock(), e.getAction(),e.getItem()))
+            );
+        }
+    }
+}
