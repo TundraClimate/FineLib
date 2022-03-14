a67c16af4107f27bd0c3e36aa20218e250c60e87
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 14:58:49 +0900

    CustomHead

---
+++ src/main/java/com/tundra/finelib/util/HeadGenerator.java
@@ -0,0 +1,41 @@
+package com.tundra.finelib.util;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+/**
+ * Gen CustomHead
+ */
+public class HeadGenerator {
+    /**
+     * Please Use https://mineskin.org/
+     * @param value skin Value
+     * @return Head
+     */
+    public static ItemStack getHead(String value) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        ItemMeta headMeta = head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", value));
+        Field profileField = null;
+        try {
+            profileField = headMeta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+        profileField.setAccessible(true);
+        try {
+            profileField.set(headMeta, profile);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(headMeta);
+        return head;
+    }
+}
