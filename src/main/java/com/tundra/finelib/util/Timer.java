4c8910b2f3b694dc60fb81798c96c4254c87476f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 18:30:46 +0900

    Timer @Deprecated

---
+++ src/main/java/com/tundra/finelib/util/Timer.java
@@ -0,0 +1,25 @@
+package com.tundra.finelib.util;
+
+import com.tundra.finelib.FineLib;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+/**
+ * Timer Class
+ */
+public class Timer {
+    /**
+     * Create Timer
+     * @param time time second
+     * @param task runner Task
+     * @return Timer
+     */
+    public static BukkitTask createTimer(double time, Runnable task){
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                FineLib.syncRunTask(task);
+            }
+        }.runTaskLater(FineLib.getPlugin(), (long)time*20);
+    }
+}
