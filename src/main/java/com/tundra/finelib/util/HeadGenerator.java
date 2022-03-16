9311fc794c36c39fb1ba65281145f9f71e7af36f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 18:28:11 +0900

    Generate CustomHead

--- src/main/java/com/tundra/finelib/util/HeadGenerator.java
+++ src/main/java/com/tundra/finelib/util/HeadGenerator.java
@@ -38,8 +38,43 @@ public class HeadGenerator {
         head.setItemMeta(headMeta);
         return head;
     }
+
+    /**
+     * get Head from UUID
+     * @param value skin Value
+     * @param uuid UUID
+     * @return Head
+     */
     public static ItemStack getHead(String value, String uuid) {
         ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        ItemMeta headMeta = head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.fromString(uuid), null);
+        profile.getProperties().put("textures", new Property("textures", value));
+        Field profileField = null;
+        try {
+            profileField = headMeta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+        profileField.setAccessible(true);
+        try {
+            profileField.set(headMeta, profile);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(headMeta);
+        return head;
+    }
+
+    /**
+     * get Head to ItemStack from UUID
+     * @param head Head ItemStack
+     * @param value skin Value
+     * @param uuid UUID
+     * @return Head
+     */
+    public static ItemStack getHead(ItemStack head, String value, String uuid) {
+        if (head.getType() != Material.PLAYER_HEAD) return head;
         ItemMeta headMeta = head.getItemMeta();
         GameProfile profile = new GameProfile(UUID.fromString(uuid), null);
         profile.getProperties().put("textures", new Property("textures", value));
