be51c6076273fe72c679a73f8b2ed02a56ed1d25
BuildTools <unconfigured@null.spigotmc.org>
2022-03-18 00:34:37 +0900

    bug fix

--- src/main/java/com/tundra/finelib/event/SignClickEvent.java
+++ src/main/java/com/tundra/finelib/event/SignClickEvent.java
@@ -4,37 +4,34 @@ import com.tundra.finelib.FineLib;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * Event of Player SignClicked
  */
-public class SignClickEvent extends PlayerEvent implements Cancellable {
-    private Sign sign = null;
-    private final ItemStack item;
-    private final Action action;
+public class SignClickEvent extends PlayerInteractEvent implements Cancellable {
     private static final HandlerList HANDLERS_LIST = new HandlerList();
     private boolean isCancelled;
 
     /**
-     * callEvent
-     * @param who clicked Player
-     * @param sign clicked Sign
-     * @param action clicked Action
-     * @param item clicked Item
+     * call Event
+     * @param who player
+     * @param action action
+     * @param item item
+     * @param clickedBlock block
+     * @param clickedFace clicked blockface
+     * @param hand clicked Hand
      */
-    public SignClickEvent(Player who, Block sign, Action action, ItemStack item) {
-        super(who);
-        if (!sign.getLocation().getBlock().isEmpty())
-            this.sign = (Sign) sign.getState();
-        this.action = action;
-        this.item = item;
+    public SignClickEvent(Player who, Action action, ItemStack item, Block clickedBlock, BlockFace clickedFace, EquipmentSlot hand) {
+        super(who, action, item, clickedBlock, clickedFace, hand);
     }
 
     @Override
@@ -48,35 +45,33 @@ public class SignClickEvent extends PlayerEvent implements Cancellable {
      */
     public static HandlerList getHandlerList() {return HANDLERS_LIST;}
 
-    @Override
-    public boolean isCancelled() {return isCancelled;}
 
     @Override
-    public void setCancelled(boolean b) {this.isCancelled = b;}
+    public void setCancelled(boolean b) {super.setCancelled(b);}
 
     /**
      * Sign getter
      * @return sign
      */
-    public Sign getSign() {return sign;}
+    public Sign getSign() {return (Sign)super.getClickedBlock();}
 
     /**
      * Action getter
      * @return Action
      */
-    public Action getAction() {return action;}
+    public Action getAction() {return super.getAction();}
 
     /**
      * Item getter
      * @return Item
      */
-    public ItemStack getItem() {return item;}
+    public ItemStack getItem() {return super.getItem();}
 
     /**
      * Material getter
      * @return Material
      */
-    public Material getMaterial() {return item.getType();}
+    public Material getMaterial() {return super.getMaterial();}
 
     /**
      * set SignClicked Task
