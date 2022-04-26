186443b021c394f7eb04f6f0570c1208cf30b1a6
BuildTools <unconfigured@null.spigotmc.org>
2022-04-26 22:20:33 +0900

    Update Addon

--- src/main/java/io/github/tundraclimate/finelib/addon/server/RegisterEvent.java
+++ src/main/java/io/github/tundraclimate/finelib/addon/server/RegisterEvent.java
@@ -3,10 +3,15 @@ package io.github.tundraclimate.finelib.addon.server;
 import io.github.tundraclimate.finelib.FineLib;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Implemented Register Method
  */
 public abstract class RegisterEvent implements Listener {
+    private static final List<RegisterEvent> register = new ArrayList<>();
+
     /**
      * Instance Register
      */
@@ -21,5 +26,9 @@ public abstract class RegisterEvent implements Listener {
      */
     public static void register(RegisterEvent event) {
         FineLib.getPlugin().getServer().getPluginManager().registerEvents(event, FineLib.getPlugin());
+    }
+
+    public static void registerAll() {
+        register.forEach(reg -> reg.register());
     }
 }
